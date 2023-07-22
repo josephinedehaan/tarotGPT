0635b7782d3bd840adcff3ca9179a12a567b47d6
josephinedehaan <josephine@bdeh.net>
2023-07-22 15:35:36 +0100

    created new chat() function in gpt_api which allows for converation with user using a session log

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, jsonify
-from gpt_api import fetch_tarot_reading
+from flask import Flask, render_template, request, jsonify, session
+from gpt_api import fetch_tarot_reading, chat
 
 app = Flask(__name__)
+app.secret_key = 'secret'
 
 @app.route('/')
 def index():
@@ -22,13 +23,26 @@ def gpt():
     cardList = [card['card'] for card in selected_cards]
 
     output_message = fetch_tarot_reading(cardList)
-
     return jsonify(output_message=output_message)  # Return the JSON response
+
+
+@app.route('/chat', methods=['POST'])  # Accept POST requests for '/gpt'
+def chatf():
+        data = request.get_json()  # Get the JSON data sent from JavaScript
+
+        message = data.get('message', None) 
+
+        output_message = chat(message)
+        return jsonify(output_message=output_message)  # Return the JSON response
+
 
 
 @app.route('/test')
 def test():
-        return "<h1>Test</h1>"
+        counter = session.get('counter', 0)
+        counter += 1
+        session['counter'] = counter
+        return f'Counter: {counter}'
 
 if __name__ == '__main__':
     app.run(debug=True)
